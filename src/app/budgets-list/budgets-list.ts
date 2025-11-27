559db180033be1d3d02225f80272001afed3a960
Jordi Miravet <jordimiravet.dev@gmail.com>
2025-11-27 19:48:37 +0100

    feat(budgets-list): implement reactive @for loop in budgets-list to display budget-item components

--- src/app/budgets-list/budgets-list.ts
+++ src/app/budgets-list/budgets-list.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BudgetItemComponent } from "../budget-item/budget-item";
+import { BudgetService } from '../services/budget';
+import { BudgetItem } from '../models/budget-item.model';
 
 @Component({
   selector: 'app-budgets-list',
   standalone: true,
   imports: [ BudgetItemComponent ],
   templateUrl: './budgets-list.html',
-  styleUrl: './budgets-list.css',
+  styleUrls: ['./budgets-list.css'],
 })
 export class BudgetsList {
+  
+  budgets = signal<BudgetItem[]>([]);
 
+  constructor(private budgetService: BudgetService){
+    this.budgets.set(this.budgetService.budgets);
+  }
 
   
 }
